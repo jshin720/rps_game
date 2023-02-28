145499125417b6fd0c85134819ab14fa9398d3c5
jshin720 <jeffshin720@gmail.com>
2023-02-28 13:50:44 -0500

    working on logic for the player vs computer

--- src/components/main_card/main_card.jsx
+++ src/components/main_card/main_card.jsx
@@ -8,24 +8,34 @@ function MainCard() {
     "Scissor",
   ]);
   const [selected, setSelected] = useState(false);
-  const [playerSelection, setPlayerSelection] = useState("");
   const [score, setScore] = useState(0);
 
-  // const update = (e) => {
-  //   setPlayerChoice(e.target.value)
-  // };
+  const update = (e) => {
+    setPlayerChoice(e.target.value);
+    setSelected = useState(true);
+  };
+
+  const randomizeComputerChoice = () => {
+    computerChoice = 
+  }
+
+  const playerWins = (playerChoice, computerChoice) => {
+    if (playerChoice === "paper" && computerChoice === "rock") {
+      //return winners page
+    }
+  }
 
   return <div>
     <div className="paper-tile">
-      <button id="paper-button" type="button">Paper</button>
+      <button id="paper-button" type="button" value="paper" onClick={update}>Paper</button>
     </div>
 
     <div className="scissor-tile">
-      <button id="scissor-button" type="button">Scissor</button>
+      <button id="scissor-button" type="button" value="scissor" onClick={update}>Scissor</button>
     </div>
 
     <div className="rock-tile">
-      <button id="rock-button" type="button">Rock</button>
+      <button id="rock-button" type="button" value="rock" onClick={update}>Rock</button>
     </div>
 
   </div>;
