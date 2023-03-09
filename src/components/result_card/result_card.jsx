7d16898de13a824e1c500eb826537c0208eb14fa
jshin720 <jeffshin720@gmail.com>
2023-03-09 18:17:35 -0500

    more factoring of components

--- src/components/result_card/result_card.jsx
+++ src/components/result_card/result_card.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
-export default function result_card() {
+export default function result_card(props) {
 
-
+    const winOrLose = (winner) => {
+      console.log("winner", winner);
+      return (
+        <div>
+          <div>
+            <h4>You Picked</h4>
+            <span>{`${pChoice}`}</span>
+          </div>
+          <div>
+            <h1>{`You ${winner}`}</h1>
+            <button>Play Again</button>
+          </div>
+          <div>
+            <h4>The House Picked</h4>
+            <span>{`${cChoice.name}`}</span>
+          </div>
+        </div>
+      );
+    };
 
   return (
     <div>
--- src/components/selection/selection.jsx
+++ src/components/selection/selection.jsx
@@ -3,28 +3,10 @@ import React, { useState, useEffect } from "react";
 function Selection(props) {
   let {pChoice, choices, score, setScore, setEnded} = props;
 
-  console.log(score++)
+  
 
   
-  const winOrLose = (winner) => {
-    console.log("winner", winner);
-      return (
-        <div>
-          <div>
-            <h4>You Picked</h4>
-            <span>{`${pChoice}`}</span>
-          </div>
-          <div>
-            <h1>{`You ${winner}`}</h1>
-            <button>Play Again</button>
-          </div>
-          <div>
-            <h4>The House Picked</h4>
-            <span>{`${cChoice.name}`}</span>
-          </div>
-        </div>
-      );
-  };
+
 
   return (
     <>
