130ff34ef95e02dea854a9c32f819e1a8c9e6043
jshin720 <jeffshin720@gmail.com>
2023-03-30 15:04:24 -0400

    fixing bug in result component

--- src/components/result_card/result_card.jsx
+++ src/components/result_card/result_card.jsx
@@ -1,20 +1,19 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 export default function ResultCard(props) {
   const [winner, setWinner] = useState("");
   const { playerChoice, computerChoice, choices, score } = props;
-  
-  console.log("choices", choices)
+
+  console.log("choices", choices);
   const pSelection = choices.find((choice) => playerChoice === choice.name);
-   console.log("ps", pSelection.beats === computerChoice.name);
-   console.log("result props", props)
-   
+  console.log("ps", pSelection.beats === computerChoice.name);
+  console.log("result props", props);
 
   useEffect(() => {
     // if (!winner) {
     //   playerWins();
     // }
-    playerWins()
+    playerWins();
     console.log(
       "pchoice",
       playerChoice,
@@ -23,14 +22,12 @@ export default function ResultCard(props) {
       "pSelction",
       pSelection
     );
-  },[])
+  }, []);
 
-
-   const playerWins = (pSelection, playerChoice, computerChoice) => {
-     
+  const playerWins = (pSelection, playerChoice, computerChoice) => {
     if (pSelection.beats === computerChoice.name) {
       setWinner("Won");
-      setScore(props.score.current + 1);
+      score.current + 1;
     } else if (playerChoice === computerChoice.name) {
       setWinner("Draw");
     } else if (computerChoice.beats === playerChoice) {
@@ -38,29 +35,30 @@ export default function ResultCard(props) {
     }
   };
   const handleClick = () => {
-   
     props.setSwitchCard("main");
   };
 
   // const winOrLose = (winner) => {
   //   console.log("winner", winner);
   //   return (
-      
+
   //   );
   // };
 
-  return <div>
-        <div>
-          <h4>You Picked</h4>
-          <span>{`${playerChoice}`}</span>
-        </div>
-        <div>
-          <h1>{`You ${winner}`}</h1>
-          <button onClick={handleClick}>Play Again</button>
-        </div>
-        <div>
-          <h4>The House Picked</h4>
-          <span>{`${computerChoice.name}`}</span>
-        </div>
-      </div>;
+  return (
+    <div>
+      <div>
+        <h4>You Picked</h4>
+        <span>{`${playerChoice}`}</span>
+      </div>
+      <div>
+        <h1>{`You ${winner}`}</h1>
+        <button onClick={handleClick}>Play Again</button>
+      </div>
+      <div>
+        <h4>The House Picked</h4>
+        <span>{`${computerChoice.name}`}</span>
+      </div>
+    </div>
+  );
 }
