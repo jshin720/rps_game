c5a4ef1ff7f9ba7660e7d32ed7bc5cc53521af86
jshin720 <jeffshin720@gmail.com>
2023-05-22 16:41:01 -0400

    refactoring code from scratch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import {
   BrowserRouter,
   BrowserRouter as Router,
@@ -14,41 +14,38 @@ import ResultsCard from "./components/result_card/result_card";
 import HomeCard from "./components/home_card/home_card";
 
 function App() {
-  const [userChoice, setUserChoice] = useState('');
-  const [computerChoice, setComputerChoice] = useState('');  
-  const [userPoints, setUserPoints] = useState(0);  
-  const [computerPoints, setComputerPoints] = useState(0);  
-  const [turnResults, setTurnResults] = useState("");  
-  const [result, setResult] = useState('');  
-  const [gameOver, setGameOver] = useState(false); 
-  
-   const choices = [
-     {
-       name: "paper",
-       beats: "rock",
-     },
-     {
-       name: "scissor",
-       beats: "paper",
-     },
-     {
-       name: "rock",
-       beats: "scissor",
-     },
-   ];
+  const [userChoice, setUserChoice] = useState("");
+  const [computerChoice, setComputerChoice] = useState("");
+  const [userPoints, setUserPoints] = useState(0);
+  const [computerPoints, setComputerPoints] = useState(0);
+  const [turnResults, setTurnResults] = useState("");
+  const [result, setResult] = useState("");
+  const [gameOver, setGameOver] = useState(false);
 
+  const choices = [
+    {
+      name: "paper",
+      beats: "rock",
+    },
+    {
+      name: "scissor",
+      beats: "paper",
+    },
+    {
+      name: "rock",
+      beats: "scissor",
+    },
+  ];
 
   return (
     <div className="App">
-      <h1 className="heading"> Rock Paper Scissor</h1>
-      <div className="score">
-        <ScoreCard
-         userPoints={userPoints}
-         computerPoints={computerPoints}
-        ></ScoreCard>
-      </div>
+      <ScoreCard
+        userPoints={userPoints}
+        computerPoints={computerPoints}
+      ></ScoreCard>
+      <HomeCard/>
 
-{/*       
+      {/*       
       <Routes>
         <Route path="/" element={<HomeCard/>}/>
         <Route path="/mainCard" element={<MainCard/>} />
