d228f4ed28002da756136c992681649b01f1acfd
jshin720 <jeffshin720@gmail.com>
2023-06-07 14:16:55 -0400

    working css for modal

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -2,21 +2,24 @@ import React from "react";
 import CloseIcon from "../../images/icon-close.svg";
 import Rules from "../../images/image-rules.svg";
 import { ReactSVG } from "react-svg";
-import "./modal.css"
+import "./modal.css";
+
 export default function modal({ openModal, onClose }) {
   if (!openModal) return null;
 
   return (
-    <div className="overlay">
-      <div className="rules-modal">
-        <span className="modal-header">
-          <h1>Rules</h1>
+    <>
+      <div className="overlay">
+        <div className="rules-modal">
+          <span className="modal-header">
+            <h1>Rules</h1>
             <ReactSVG src={CloseIcon} onClick={onClose} />
-        </span>
-        <div className="beats-container">
-          <ReactSVG src={Rules} />
+          </span>
+          <div className="beats-container">
+            <ReactSVG src={Rules} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
